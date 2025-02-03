ba78262734e6ee341a913ddbfa259c426c54ce47
mica-oz <kahulaoz@gmail.com>
2025-02-03 11:46:06 -0800

    removing dashboard dir for now

--- app/(dashboard)/dashboard/users/[id]/page.tsx
+++
@@ -1,8 +0,0 @@
-// import React from "react";
-
-// const Profile = ({ params }: { params: { id: string } }) => {
-//   const { id } = params;
-//   return <h1 className="text-3xl">User Profile: {id}</h1>;
-// };
-
-// export default Profile;
