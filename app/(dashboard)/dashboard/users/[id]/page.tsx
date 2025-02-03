2ca381ffa58a64d52873f99b9544601050150eaa
mica-oz <kahulaoz@gmail.com>
2025-02-03 11:40:41 -0800

    removing the user profile component for now

--- app/(dashboard)/dashboard/users/[id]/page.tsx
+++ app/(dashboard)/dashboard/users/[id]/page.tsx
@@ -1,8 +1,8 @@
-import React from "react";
+// import React from "react";
 
-const Profile = ({ params }: { params: { id: string } }) => {
-  const { id } = params;
-  return <h1 className="text-3xl">User Profile: {id}</h1>;
-};
+// const Profile = ({ params }: { params: { id: string } }) => {
+//   const { id } = params;
+//   return <h1 className="text-3xl">User Profile: {id}</h1>;
+// };
 
-export default Profile;
+// export default Profile;
