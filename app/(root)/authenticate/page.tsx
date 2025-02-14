5ba810cf1f506d5a74c8cc9c0066283e8fd53761
mica-oz <kahulaoz@gmail.com>
2025-02-14 05:07:04 -0800

    dash

--- app/(root)/authenticate/page.tsx
+++ app/(root)/authenticate/page.tsx
@@ -1,10 +1,12 @@
 // import React, { useEffect } from "react";
 // import { useStytch, useStytchSession } from "@stytch/nextjs";
 import FooterBlock from "@/app/components/footerBlock";
+import Dash1 from "@/app/components/dashboard/status1";
 
 const Authenticate = () => {
   return (
     <div>
+      <Dash1 />
       <FooterBlock />
     </div>
   );
---
+++ app/components/dashboard/status1.tsx
@@ -0,0 +1,168 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import scales from "@/public/scales-icon.png";
+const Dash = () => {
+  return (
+    <div className="dash-cont">
+      <div className="row-1">
+        <p className="dash-greet">
+          Welcome to your Dashboard, <strong>Test!</strong>
+        </p>
+      </div>
+      <div className="row-2">
+        <div className="progress-bubble">
+          <div className="bubble-header">Tax History Report Progress</div>
+          <div className="square-front">
+            <div className="circle-cont">
+              <div className="outer-circle">
+                <div className="inner-circle"></div>
+              </div>
+              <div className="outer-circle"></div>
+
+              <div className="outer-circle"></div>
+
+              <div className="outer-circle"></div>
+
+              <div className="outer-circle"></div>
+
+              <div className="outer-circle"></div>
+
+              <div className="outer-circle"></div>
+            </div>
+            <div className="line-cont"></div>
+            <div className="status-bubble">
+              <p>Request Tax History Report</p>
+            </div>
+          </div>
+          <div className="bubble-header-back"></div>
+          <div className="square-back"></div>
+        </div>
+
+        <div className="to-do-bubble">
+          <div className="square-front">
+            <p className="to-do-head">To Do:</p>
+            <p className="to-do-msg">
+              Fill out Tax History Report Request Form{" "}
+            </p>
+            <div className="tax-history-req-btn">
+              REQUEST TAX <br />
+              HISTORY REPORT
+            </div>
+          </div>
+          <div className="square-back"></div>
+        </div>
+      </div>
+      <div className="row-3 bar-bubble">
+        <div className="square-front">
+          <p>Wondering what a Tax History Report will show you?</p>
+          <Link href={"/about"} style={{ marginLeft: "auto" }}>
+            <div className="learn-more-btn">LEARN MORE</div>
+          </Link>
+        </div>
+        <div className="square-back"></div>
+      </div>
+      <div className="row-4">
+        <div className="detail-bubble">
+          <div className="bubble-header" style={{ fontSize: "30px" }}>
+            Current Liability
+          </div>
+
+          <div className="square-front">
+            <p>
+              Awaiting
+              <br />
+              Tax
+              <br />
+              History
+              <br /> Report
+            </p>
+          </div>
+          <div className="bubble-header-back"></div>
+          <div className="square-back"></div>
+        </div>
+        <div className="detail-bubble">
+          <div className="bubble-header" style={{ fontSize: "30px" }}>
+            Years Unfiled
+          </div>
+
+          <div className="square-front">
+            <p>
+              Awaiting
+              <br />
+              Tax
+              <br />
+              History
+              <br /> Report
+            </p>
+          </div>
+          <div className="bubble-header-back"></div>
+          <div className="square-back"></div>
+        </div>
+        <div className="detail-bubble">
+          <div className="bubble-header" style={{ fontSize: "30px" }}>
+            Payment Status
+          </div>
+
+          <div className="square-front">
+            <p>
+              Awaiting
+              <br />
+              Tax
+              <br />
+              History
+              <br /> Report
+            </p>
+          </div>
+          <div className="bubble-header-back"></div>
+          <div className="square-back"></div>
+        </div>
+      </div>
+      <div className="row-5 bar-bubble">
+        <div className="square-front">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="50"
+            height="50"
+            fill="#0a1763"
+            className="bi bi-speedometer score-icon"
+            viewBox="0 0 16 16"
+          >
+            <path d="M8 2a.5.5 0 0 1 .5.5V4a.5.5 0 0 1-1 0V2.5A.5.5 0 0 1 8 2M3.732 3.732a.5.5 0 0 1 .707 0l.915.914a.5.5 0 1 1-.708.708l-.914-.915a.5.5 0 0 1 0-.707M2 8a.5.5 0 0 1 .5-.5h1.586a.5.5 0 0 1 0 1H2.5A.5.5 0 0 1 2 8m9.5 0a.5.5 0 0 1 .5-.5h1.5a.5.5 0 0 1 0 1H12a.5.5 0 0 1-.5-.5m.754-4.246a.39.39 0 0 0-.527-.02L7.547 7.31A.91.91 0 1 0 8.85 8.569l3.434-4.297a.39.39 0 0 0-.029-.518z" />
+            <path
+              fillRule="evenodd"
+              d="M6.664 15.889A8 8 0 1 1 9.336.11a8 8 0 0 1-2.672 15.78zm-4.665-4.283A11.95 11.95 0 0 1 8 10c2.186 0 4.236.585 6.001 1.606a7 7 0 1 0-12.002 0"
+            />
+          </svg>
+          <p>Check your credit score for free while you wait!</p>
+          <Link href={"/credit"} style={{ marginLeft: "auto" }}>
+            <div className="learn-more-btn">GET MY SCORE</div>
+          </Link>
+        </div>
+        <div className="square-back"></div>
+      </div>
+      <div className="row-6 bar-bubble">
+        <div className="square-front">
+          <Image alt={"icon"} src={scales} width={60} className="scale-icon" />
+          <p>
+            Looking for Tax Resolution? Schedule a{" "}
+            <span style={{ color: "#59c8ea" }}>
+              {" "}
+              <strong>
+                free 15 minute <br />
+                consultation{" "}
+              </strong>
+            </span>
+            with our team of tax lawyers.
+          </p>
+          <Link href={"/contact"} style={{ marginLeft: "auto" }}>
+            <div className="learn-more-btn">SCHEDULE NOW</div>
+          </Link>
+        </div>
+        <div className="square-back"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Dash;
