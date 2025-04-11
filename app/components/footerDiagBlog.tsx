e6263058178aa2ac87af38eb4e60a177f72911f1
mica-oz <kahulaoz@gmail.com>
2025-04-11 14:30:47 -0700

    blog BG adjust

--- app/components/footerDiagBlog.tsx
+++ app/components/footerDiagBlog.tsx
@@ -100,33 +100,37 @@ const FooterDiag = () => {
             </div>
           </div>
           <div className="row-5">
-            <div className="footer-group group1">
-              <h5 className="section-cat">Navigation</h5>
-              <p>About</p>
-              <p>Tax Resolution</p>
-              <p>Blog</p>
-              <p>FAQ</p>
-              <p>Contact</p>
+            <div className="sub-row sub-row-1">
+              <div className="footer-group group1">
+                <h5 className="section-cat">Navigation</h5>
+                <p>About</p>
+                <p>Tax Resolution</p>
+                <p>Blog</p>
+                <p>FAQ</p>
+                <p>Contact</p>
+              </div>
+              <div className="footer-group group2">
+                <h5 className="section-cat">Company Information</h5>
+                <p>Our Mission & Culture</p>
+                <p>Careers</p>
+                <p>Partnerships</p>
+                <p>Security</p>
+                <p>Terms & Conditions</p>
+                <p>Privacy Policy</p>
+              </div>
             </div>
-            <div className="footer-group group2">
-              <h5 className="section-cat">Company Information</h5>
-              <p>Our Mission & Culture</p>
-              <p>Careers</p>
-              <p>Partnerships</p>
-              <p>Security</p>
-              <p>Terms & Conditions</p>
-              <p>Privacy Policy</p>
-            </div>
-            <div className="footer-group group3">
-              <h5 className="section-cat">Services</h5>
-              <p>Free Tax History</p>
-              <p>Tax Resolution</p>
-              <p>Free Credit Report</p>
-            </div>
-            <div className="footer-group group4">
-              <h5 className="section-cat">Support</h5>
-              <p>Customer Success</p>
-              <p>FAQ</p>
+            <div className="sub-row sub-row-2">
+              <div className="footer-group group3">
+                <h5 className="section-cat">Services</h5>
+                <p>Free Tax History</p>
+                <p>Tax Resolution</p>
+                <p>Free Credit Report</p>
+              </div>
+              <div className="footer-group group4">
+                <h5 className="section-cat">Support</h5>
+                <p>Customer Success</p>
+                <p>FAQ</p>
+              </div>
             </div>
           </div>
           <div className="row-6">
