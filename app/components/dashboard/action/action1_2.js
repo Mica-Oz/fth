6c1ca60de5c8e4e02937bb363f0f80acf28850f9
mica-oz <kahulaoz@gmail.com>
2025-04-23 10:31:31 -0700

    gtm event updates

--- app/components/dashboard/action/action1_2.js
+++ app/components/dashboard/action/action1_2.js
@@ -8,7 +8,6 @@ import { useRouter } from "next/navigation";
 import { useAppContext } from "@/app/context";
 import updateStatus from "@/app/utilities/api/updateStatus";
 import getLogicsUser from "@/app/utilities/api/getLogicsUser";
-import { sendGTMEvent } from "@next/third-parties/google";
 
 const Action1_2 = () => {
   const router = useRouter();
@@ -225,11 +224,7 @@ const Action1_2 = () => {
 
     try {
       await submitForm(); // Wait for form submission to complete
-      //  Fire GTM event only after successful form submission
-      sendGTMEvent({
-        event: "formSubmitted",
-        value: "Tax Report Request Form Submission",
-      });
+
       router.push("/dashboard/status2"); // Then navigate programmatically
     } catch (error) {
       console.error("Error details:", {
@@ -325,10 +320,14 @@ const Action1_2 = () => {
                 Terms & Condtitions
               </span>
             </p>
+            <button
+              onClick={handleSubmit}
+              className="next-btn"
+              id="thrReqSubmit"
+            >
+              SUBMIT
+            </button>
           </div>
-          <button onClick={handleSubmit} className="next-btn">
-            SUBMIT
-          </button>
         </div>
         <div className="header-bubble-back"></div>
         <div className="back-square"></div>
