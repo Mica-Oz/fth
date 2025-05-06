ebfbef9edbd4f16b87ce08a265afe74d61ec10ed
mica-oz <kahulaoz@gmail.com>
2025-05-06 11:14:47 -0700

    spouse1-1 validation

--- app/schema/action1_1_Schema_Spouse.tsx
+++ app/schema/action1_1_Schema_Spouse.tsx
@@ -14,25 +14,25 @@ const dobPattern = /^(0[1-9]|1[0-2])\/(0[1-9]|[12]\d|3[01])\/\d{4}$/;
 const ssnPattern = /^\d{3}-?\d{2}-?\d{4}$/;
 
 export const action1_1_Spouse_Schema = z.object({
-  FirstName: z
+  sfname: z
     .string()
     .min(1, { message: "First name is required" })
     .regex(/^[A-Za-z\s'-]+$/, {
       message: "First name must not contain numbers or symbols",
     })
     .transform((val) => toTitleCase(val)),
-  LastName: z
+  slname: z
     .string()
     .min(1, { message: "Last name is required" })
     .regex(/^[A-Za-z\s'-]+$/, {
       message: "Last name must not contain numbers or symbols",
     })
     .transform((val) => toTitleCase(val)),
-  dob: z
+  sdob: z
     .string()
     .min(1, { message: "Date of birth is required" })
     .regex(dobPattern, { message: "Date must be in MM/DD/YYYY format" }),
-  ssn: z
+  sssn: z
     .string()
     .min(1, { message: "SSN is required" })
     .regex(ssnPattern, { message: "SSN must be in XXX-XX-XXXX format" })
@@ -43,27 +43,27 @@ export const action1_1_Spouse_Schema = z.object({
       }
       return val;
     }),
-  CellPhone: z
-    .string()
-    .optional()
-    .refine(
-      (val) => {
-        // If value is undefined or empty string, it's valid
-        if (!val) return true;
+  // CellPhone: z
+  //   .string()
+  //   .optional()
+  //   .refine(
+  //     (val) => {
+  //       // If value is undefined or empty string, it's valid
+  //       if (!val) return true;
 
-        // Otherwise apply validation
-        const digitsOnly = val.replace(/\D/g, "");
-        return digitsOnly.length === 10 && /^\d+$/.test(digitsOnly);
-      },
-      {
-        message: "Phone number must be 10 digits",
-      }
-    )
-    .transform((val) => {
-      // If no value, return undefined
-      if (!val) return undefined;
+  //       // Otherwise apply validation
+  //       const digitsOnly = val.replace(/\D/g, "");
+  //       return digitsOnly.length === 10 && /^\d+$/.test(digitsOnly);
+  //     },
+  //     {
+  //       message: "Phone number must be 10 digits",
+  //     }
+  //   )
+  //   .transform((val) => {
+  //     // If no value, return undefined
+  //     if (!val) return undefined;
 
-      // Otherwise sanitize to just digits
-      return val.replace(/\D/g, "");
-    }),
+  //     // Otherwise sanitize to just digits
+  //     return val.replace(/\D/g, "");
+  //   }),
 });
