9279875337c8b995379d8d609c48a83246586514
mica-oz <kahulaoz@gmail.com>
2025-05-12 15:03:22 -0700

    eligibility loop added

--- app/components/dashboard/action/action3_2.tsx
+++ app/components/dashboard/action/action3_2.tsx
@@ -187,10 +187,11 @@ const Action = () => {
               </select>
             </div>
           </form>
-
-          <button onClick={submit} className="next-btn">
-            Next
-          </button>
+          <div className="action-btn-cont">
+            <button onClick={submit} className="next-btn">
+              Next
+            </button>
+          </div>
         </div>
         <div className="header-bubble-back"></div>
         <div className="back-square"></div>
