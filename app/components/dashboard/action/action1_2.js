89ca74a322b72990380f621d2666b2d5e2b96b71
mica-oz <kahulaoz@gmail.com>
2025-05-13 14:26:12 -0700

    sig

--- app/components/dashboard/action/action1_2.js
+++ app/components/dashboard/action/action1_2.js
@@ -313,9 +313,14 @@ const Action1_2 = () => {
           <p className="sub-heading">
             <strong>You&apos;re almost done!</strong>
             <br />
-            Just E-sign below and we will begin generating your Free Tax History
-            Report!
+            Sign below and we will begin generating your Free Tax History
+            Report! <br />
+            <strong>
+              Note: This is a legal document. Please sign carefully{" "}
+              <u>on the line</u> (as it appears on your most recent tax return).
+            </strong>
           </p>
+
           <form
             onSubmit={onSubmit}
             className="form-cont"
@@ -328,6 +333,18 @@ const Action1_2 = () => {
                   <div className="signature-pad--body">
                     <div style={{ display: isSubmitting ? "none" : "block" }}>
                       <SignatureCanvas ref={sigCanvas} />
+                      <div className="sign-line">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="20"
+                          height="20"
+                          fill="#0a1763"
+                          className="bi bi-x-lg"
+                          viewBox="0 0 16 16"
+                        >
+                          <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z" />
+                        </svg>
+                      </div>
                     </div>
                   </div>
                   {signatureError && (
