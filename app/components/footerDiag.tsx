69e5e19808120d58c8f4d8d6c4a73f201060161b
mica-oz <kahulaoz@gmail.com>
2025-05-13 12:23:55 -0700

    careers and partnerships pages added

--- app/components/footerDiag.tsx
+++ app/components/footerDiag.tsx
@@ -135,11 +135,12 @@ const FooterDiag = ({ page }: FooterDiagProps) => {
                   <Link href="/">Our Mission & Culture</Link>
                 </p>
                 <p>
-                  <Link href="/">Careers</Link>
+                  <Link href="/careers">Careers</Link>
                 </p>
                 <p>
-                  <Link href="/">Partnerships</Link>
+                  <Link href="/partnerships">Partnerships</Link>
                 </p>
+
                 {/* 
                 <p>
                   <Link href="/security">Security</Link>
