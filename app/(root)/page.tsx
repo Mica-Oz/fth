5fe2a9c5205e45e5d9d6b6dcfe8b49ee89569095
mica-oz <kahulaoz@gmail.com>
2025-05-14 14:54:55 -0700

    header copy change

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -164,8 +164,8 @@ export default function Home() {
           <p data-aos="fade-left" data-aos-delay="150">
             Your confidential, Free Tax History Report will clearly summarize a
             10+ year history of your IRS Individual Master File. We can identify
-            any potential issues and help you find resolution safely and
-            securely.
+            the current balance, any open issues and help you find a solution
+            safely and securely.
           </p>
           <Link href={"/signup"}>
             <div
