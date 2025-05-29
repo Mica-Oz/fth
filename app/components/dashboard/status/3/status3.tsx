fc97856bed11e33b14bccf58acbf5f13f3b7da4a
mica-oz <kahulaoz@gmail.com>
2025-05-29 12:54:54 -0700

    no activity errors status 3

--- app/components/dashboard/status/3/status3.tsx
+++ app/components/dashboard/status/3/status3.tsx
@@ -155,7 +155,7 @@ const Dash = () => {
           </div>
 
           <div className="square-front">
-            <p className="active">{userData?.currentLiability}</p>
+            <p className="active">{userData?.currentLiability || ""}</p>
           </div>
           <div className="bubble-header-back"></div>
           <div className="square-back"></div>
@@ -167,7 +167,7 @@ const Dash = () => {
 
           <div className="square-front">
             <p className="active">
-              {userData?.yearsUnfiled}
+              {userData?.yearsUnfiled || ""}
               <br />
               Unfiled
             </p>
@@ -182,7 +182,7 @@ const Dash = () => {
 
           <div className="square-front">
             <p className="active">
-              {userData?.paymentStatus}
+              {userData?.paymentStatus || ""}
               {/* Unpaid
               <br />
               -
