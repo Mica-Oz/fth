2fd5beadfa796fede8c726a134fd9ccbc2055cb0
mica-oz <kahulaoz@gmail.com>
2025-07-15 11:29:10 -0700

    loading message'

--- app/components/dashboard/action/action2.tsx
+++ app/components/dashboard/action/action2.tsx
@@ -143,7 +143,7 @@ const Action = () => {
               }}
             >
               {isLoading ? (
-                <div>Loading PDF...</div>
+                <div>Loading Report...</div>
               ) : signedURL ? (
                 <button
                   onClick={() => window.open(signedURL, "_blank")}
